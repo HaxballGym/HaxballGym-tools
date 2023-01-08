570df63a3444d9fc5f8d8feb42443af6c3f2a4af
Wazarr <jeje_04@live.com>
2023-01-08 15:26:49 +0100

    v1.0.2

--- haxballgym_tools/examples/sb3_single_example.py
+++ haxballgym_tools/examples/sb3_single_example.py
@@ -1,11 +1,14 @@
-from haxballgym import make
-from haxballgym.game import Game
-from haxballgym.game import common_values as cv
+from haxballgym import make, Game
+from haxballgym import common_values as cv
+from haxballgym.gym import Gym
 
-from haxballgym.utils.terminal_conditions import TerminalCondition, common_conditions
-from haxballgym.utils.reward_functions import RewardFunction, CombinedReward, velocity_reward
-from haxballgym.utils.obs_builders import DefaultObs, ObsBuilder
-from haxballgym.utils.action_parsers import DefaultAction, ActionParser
+from haxballgym.utils.terminal_conditions import common_conditions
+from haxballgym.utils.reward_functions import (
+    CombinedReward,
+    velocity_reward,
+)
+from haxballgym.utils.obs_builders import DefaultObs
+from haxballgym.utils.action_parsers import DefaultAction
 
 import logging
 import numpy as np
@@ -18,27 +21,45 @@ from haxballgym_tools.sb3_utils import SB3SingleInstanceEnv
 frame_skip = 10
 half_life_seconds = 5
 
+
+def recording_condition(gym_env: Gym) -> bool:
+    # Returns whether the game should be recorded or not
+    # Change the condition if you find that too many games are being recorded
+    game = gym_env._match._game
+    # Current condition: If a goal has been scored and the time is more than 10 seconds
+    if game.score.red + game.score.blue > 0 and game.score.time > 10:
+        return True
+
+    return False
+
+
 def create_gym_env():
     game = Game(
         stadium_file=cv.MAP_CLASSIC,
-        folder_rec='./recordings/',
-        logging_level=logging.DEBUG
+        folder_rec="./recordings/",
+        logging_level=logging.DEBUG,
     )
     gym_env = make(
         game=game,
         reward_fn=CombinedReward(
             (
                 velocity_reward.VelocityPlayerToBallReward(),
-                velocity_reward.VelocityBallToGoalReward(stadium=game.stadium_store, own_goal=False)
+                velocity_reward.VelocityBallToGoalReward(
+                    stadium=game.stadium_store, own_goal=False
+                ),
             )
         ),
-        terminal_conditions=(common_conditions.TimeoutCondition(1 * 60 * 60 / frame_skip), common_conditions.GoalScoredCondition()),
+        terminal_conditions=(
+            common_conditions.TimeoutCondition(1 * 60 * 60 / frame_skip),
+            common_conditions.GoalScoredCondition(),
+        ),
         obs_builder=DefaultObs(),
         action_parser=DefaultAction(),
         team_size=1,
-        tick_skip=frame_skip
+        tick_skip=frame_skip,
     )
     return gym_env
+
 
 gym_env = create_gym_env()
 
@@ -46,30 +67,31 @@ fps = 60 / frame_skip
 gamma = np.exp(np.log(0.5) / (fps * half_life_seconds))
 
 # wrap the RLGym environment with the single instance wrapper
-env = SB3SingleInstanceEnv(gym_env)
+env = SB3SingleInstanceEnv(gym_env, recording_condition)
 env = VecCheckNan(env)
 env = VecMonitor(env)
 env = VecNormalize(env, norm_obs=False, gamma=gamma)
 
 # Save a checkpoint every 10000 steps
-checkpoint_callback = CheckpointCallback(save_freq=50_000 / env.num_envs, 
-                                         save_path='./logs/', name_prefix='hax_model_simple')
+checkpoint_callback = CheckpointCallback(
+    save_freq=50_000 / env.num_envs, save_path="./logs/", name_prefix="hax_model_simple"
+)
 
 # create a PPO instance and start learning
 model = PPO(
-        "MlpPolicy",
-        env,
-        n_epochs=32,                 # PPO calls for multiple epochs
-        learning_rate=1e-5,          # Around this is fairly common for PPO
-        ent_coef=0.01,               # From PPO Atari
-        vf_coef=1.,                  # From PPO Atari
-        gamma=gamma,                 # Gamma as calculated using half-life
-        verbose=3,                   # Print out all the info as we're going
-        batch_size=4096,             # Batch size as high as possible within reason
-        n_steps=4096,                # Number of steps to perform before optimizing network
-        tensorboard_log="./policy/", # `tensorboard --logdir policy` in terminal to see graphs
-        device="auto"                # Uses GPU if available
-    )
+    "MlpPolicy",
+    env,
+    n_epochs=32,  # PPO calls for multiple epochs
+    learning_rate=1e-5,  # Around this is fairly common for PPO
+    ent_coef=0.01,  # From PPO Atari
+    vf_coef=1.0,  # From PPO Atari
+    gamma=gamma,  # Gamma as calculated using half-life
+    verbose=3,  # Print out all the info as we're going
+    batch_size=4096,  # Batch size as high as possible within reason
+    n_steps=4096,  # Number of steps to perform before optimizing network
+    tensorboard_log="./policy/",  # `tensorboard --logdir policy` in terminal
+    device="auto",  # Uses GPU if available
+)
 
 model.learn(100_000_000, callback=checkpoint_callback)
 
@@ -77,8 +99,8 @@ model.learn(100_000_000, callback=checkpoint_callback)
 # model = PPO.load(
 #     "logs/model_file.zip",
 #     env,
-#     custom_objects=dict(n_envs=env.num_envs, _last_obs=None),  # Need this to change number of agents
-#     device="auto"  # Need to set device again (if using a specific one)
+#     custom_objects=dict(n_envs=env.num_envs, _last_obs=None),  # Number of agents
+#     device="auto",  # Need to set device again (if using a specific one)
 # )
 # env.reset()  # Important when loading models, SB3 does not do this for you
 
--- haxballgym_tools/sb3_utils/sb3_hax_single_instance.py
+++ haxballgym_tools/sb3_utils/sb3_hax_single_instance.py
@@ -1,36 +1,45 @@
-from typing import Optional, List, Union, Sequence, Type, Any
+from typing import Callable, Optional, List, Union, Sequence, Type, Any
 
 import gym
 import numpy as np
-from haxballgym.envs import Environment as HaxEnv
 from stable_baselines3.common.vec_env import VecEnv
-from stable_baselines3.common.vec_env.base_vec_env import VecEnvIndices, VecEnvStepReturn, VecEnvObs
+from stable_baselines3.common.vec_env.base_vec_env import (
+    VecEnvIndices,
+    VecEnvStepReturn,
+    VecEnvObs,
+)
 
-from haxballgym.gym import Gym
+from haxballgym.gym import Gym as GymEnv
 
-def recording_condition(env: HaxEnv) -> bool:
-    # Returns whether the game should be recorded or not
-    # Feel free to change the condition if you find that too many games are being recorded
-    if env._match._game.score.red + env._match._game.score.blue > 0:
+
+def recording_condition(gym_env: GymEnv) -> bool:
+    game = gym_env._match._game
+    if game.score.red + game.score.blue > 0:
         return True
-        
+
     return False
+
 
 class SB3SingleInstanceEnv(VecEnv):
     """
-    Class for wrapping a single haxballgym env into a VecEnv (each player is treated as its own environment).
+    Class for wrapping a single haxballgym env into a VecEnv.
     """
 
-    def __init__(self, env: Gym):
+    def __init__(
+        self,
+        env: GymEnv,
+        recording_condition: Callable[[GymEnv], bool] = recording_condition,
+    ):
         """
         :param env: the environment to wrap.
         """
         super().__init__(env._match.agents, env.observation_space, env.action_space)
         self.env = env
         self.step_result = None
+        self.recording_condition = recording_condition
 
     def reset(self) -> VecEnvObs:
-        save_recording = recording_condition(self.env)
+        save_recording = self.recording_condition(self.env)
         observations = self.env.reset(save_recording=save_recording)
         return np.asarray(observations)
 
@@ -50,12 +59,17 @@ class SB3SingleInstanceEnv(VecEnv):
             for info, obs in zip(infos, observations):
                 info["terminal_observation"] = obs
 
-            save_recording = recording_condition(self.env)
+            save_recording = self.recording_condition(self.env)
             observations = self.env.reset(save_recording=save_recording)
 
         else:
             infos = [info] * len(rewards)
-        return np.asarray(observations), np.array(rewards), np.full(len(rewards), done), infos
+        return (
+            np.asarray(observations),
+            np.array(rewards),
+            np.full(len(rewards), done),
+            infos,
+        )
 
     def close(self) -> None:
         self.env.close()
@@ -63,19 +77,28 @@ class SB3SingleInstanceEnv(VecEnv):
     def seed(self, seed: Optional[int] = None) -> List[Union[None, int]]:
         return [self.env.seed(seed)] * self.num_envs
 
-    # Now a bunch of functions that need to be overridden to work, might have to implement later
+    # Bunch of functions that need to be overridden to work, might have to implement
     def get_attr(self, attr_name: str, indices: VecEnvIndices = None) -> List[Any]:
         pass
 
-    def set_attr(self, attr_name: str, value: Any, indices: VecEnvIndices = None) -> None:
+    def set_attr(
+        self, attr_name: str, value: Any, indices: VecEnvIndices = None
+    ) -> None:
         pass
 
-    def env_method(self, method_name: str, *method_args, indices: VecEnvIndices = None, **method_kwargs) -> List[Any]:
+    def env_method(
+        self,
+        method_name: str,
+        *method_args,
+        indices: VecEnvIndices = None,
+        **method_kwargs
+    ) -> List[Any]:
         pass
 
-    def env_is_wrapped(self, wrapper_class: Type[gym.Wrapper], indices: VecEnvIndices = None) -> List[bool]:
+    def env_is_wrapped(
+        self, wrapper_class: Type[gym.Wrapper], indices: VecEnvIndices = None
+    ) -> List[bool]:
         return [False] * self.num_envs
 
     def get_images(self) -> Sequence[np.ndarray]:
         pass
-
